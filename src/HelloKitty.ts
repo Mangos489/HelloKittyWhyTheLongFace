434ccbfdbc2465dfbebe33869b63f99cc3121c3e
ecruz22004 <ecruz22004@gmail.com>
2024-05-15 21:51:13 -0700

    gradient background pink to white

--- src/HelloKitty.ts
+++ src/HelloKitty.ts
@@ -4,66 +4,66 @@ export const HelloKitty: CustomThemeConfig = {
 	name: 'HelloKitty',
 	properties: {
 		// =~= Theme Properties =~=
-		"--theme-font-family-base": "system-ui",
-		"--theme-font-family-heading": "system-ui",
+		"--theme-font-family-base": `system-ui`,
+		"--theme-font-family-heading": `system-ui`,
 		"--theme-font-color-base": "0 0 0",
-		"--theme-font-color-dark": "255 255 255",
+		"--theme-font-color-dark": "0 0 0",
 		"--theme-rounded-base": "9999px",
 		"--theme-rounded-container": "8px",
 		"--theme-border-base": "1px",
 		// =~= Theme On-X Colors =~=
 		"--on-primary": "0 0 0",
-		"--on-secondary": "255 255 255",
+		"--on-secondary": "0 0 0",
 		"--on-tertiary": "0 0 0",
-		"--on-success": "0 0 0",
+		"--on-success": "255 255 255",
 		"--on-warning": "0 0 0",
 		"--on-error": "255 255 255",
 		"--on-surface": "255 255 255",
 		// =~= Theme Colors  =~=
-		// primary | #0FBA81 
-		"--color-primary-50": "219 245 236", // #dbf5ec
-		"--color-primary-100": "207 241 230", // #cff1e6
-		"--color-primary-200": "195 238 224", // #c3eee0
-		"--color-primary-300": "159 227 205", // #9fe3cd
-		"--color-primary-400": "87 207 167", // #57cfa7
-		"--color-primary-500": "15 186 129", // #0FBA81
-		"--color-primary-600": "14 167 116", // #0ea774
-		"--color-primary-700": "11 140 97", // #0b8c61
-		"--color-primary-800": "9 112 77", // #09704d
-		"--color-primary-900": "7 91 63", // #075b3f
-		// secondary | #4F46E5 
-		"--color-secondary-50": "229 227 251", // #e5e3fb
-		"--color-secondary-100": "220 218 250", // #dcdafa
-		"--color-secondary-200": "211 209 249", // #d3d1f9
-		"--color-secondary-300": "185 181 245", // #b9b5f5
-		"--color-secondary-400": "132 126 237", // #847eed
-		"--color-secondary-500": "79 70 229", // #4F46E5
-		"--color-secondary-600": "71 63 206", // #473fce
-		"--color-secondary-700": "59 53 172", // #3b35ac
-		"--color-secondary-800": "47 42 137", // #2f2a89
-		"--color-secondary-900": "39 34 112", // #272270
-		// tertiary | #0EA5E9 
-		"--color-tertiary-50": "219 242 252", // #dbf2fc
-		"--color-tertiary-100": "207 237 251", // #cfedfb
-		"--color-tertiary-200": "195 233 250", // #c3e9fa
-		"--color-tertiary-300": "159 219 246", // #9fdbf6
-		"--color-tertiary-400": "86 192 240", // #56c0f0
-		"--color-tertiary-500": "14 165 233", // #0EA5E9
-		"--color-tertiary-600": "13 149 210", // #0d95d2
-		"--color-tertiary-700": "11 124 175", // #0b7caf
-		"--color-tertiary-800": "8 99 140", // #08638c
-		"--color-tertiary-900": "7 81 114", // #075172
-		// success | #84cc16 
-		"--color-success-50": "237 247 220", // #edf7dc
-		"--color-success-100": "230 245 208", // #e6f5d0
-		"--color-success-200": "224 242 197", // #e0f2c5
-		"--color-success-300": "206 235 162", // #ceeba2
-		"--color-success-400": "169 219 92", // #a9db5c
-		"--color-success-500": "132 204 22", // #84cc16
-		"--color-success-600": "119 184 20", // #77b814
-		"--color-success-700": "99 153 17", // #639911
-		"--color-success-800": "79 122 13", // #4f7a0d
-		"--color-success-900": "65 100 11", // #41640b
+		// primary | #FFD7FB 
+		"--color-primary-50": "255 249 254", // #fff9fe
+		"--color-primary-100": "255 247 254", // #fff7fe
+		"--color-primary-200": "255 245 254", // #fff5fe
+		"--color-primary-300": "255 239 253", // #ffeffd
+		"--color-primary-400": "255 227 252", // #ffe3fc
+		"--color-primary-500": "255 215 251", // #FFD7FB
+		"--color-primary-600": "230 194 226", // #e6c2e2
+		"--color-primary-700": "191 161 188", // #bfa1bc
+		"--color-primary-800": "153 129 151", // #998197
+		"--color-primary-900": "125 105 123", // #7d697b
+		// secondary | #FF9DF5 
+		"--color-secondary-50": "255 240 254", // #fff0fe
+		"--color-secondary-100": "255 235 253", // #ffebfd
+		"--color-secondary-200": "255 231 253", // #ffe7fd
+		"--color-secondary-300": "255 216 251", // #ffd8fb
+		"--color-secondary-400": "255 186 248", // #ffbaf8
+		"--color-secondary-500": "255 157 245", // #FF9DF5
+		"--color-secondary-600": "230 141 221", // #e68ddd
+		"--color-secondary-700": "191 118 184", // #bf76b8
+		"--color-secondary-800": "153 94 147", // #995e93
+		"--color-secondary-900": "125 77 120", // #7d4d78
+		// tertiary | #FFFEFE 
+		"--color-tertiary-50": "255 249 254", // #fff9fe
+		"--color-tertiary-100": "255 247 254", // #fff7fe
+		"--color-tertiary-200": "255 245 254", // #fff5fe
+		"--color-tertiary-300": "255 239 253", // #ffeffd
+		"--color-tertiary-400": "255 227 252", // #ffe3fc
+		"--color-tertiary-500": "255 215 251", // #FFD7FB
+		"--color-tertiary-600": "230 194 226", // #e6c2e2
+		"--color-tertiary-700": "191 161 188", // #bfa1bc
+		"--color-tertiary-800": "153 129 151", // #998197
+		"--color-tertiary-900": "125 105 123", // #7d697b
+		// success | #153acb 
+		"--color-success-50": "220 225 247", // #dce1f7
+		"--color-success-100": "208 216 245", // #d0d8f5
+		"--color-success-200": "197 206 242", // #c5cef2
+		"--color-success-300": "161 176 234", // #a1b0ea
+		"--color-success-400": "91 117 219", // #5b75db
+		"--color-success-500": "21 58 203", // #153acb
+		"--color-success-600": "19 52 183", // #1334b7
+		"--color-success-700": "16 44 152", // #102c98
+		"--color-success-800": "13 35 122", // #0d237a
+		"--color-success-900": "10 28 99", // #0a1c63
 		// warning | #EAB308 
 		"--color-warning-50": "252 244 218", // #fcf4da
 		"--color-warning-100": "251 240 206", // #fbf0ce
@@ -75,27 +75,28 @@ export const HelloKitty: CustomThemeConfig = {
 		"--color-warning-700": "176 134 6", // #b08606
 		"--color-warning-800": "140 107 5", // #8c6b05
 		"--color-warning-900": "115 88 4", // #735804
-		// error | #D41976 
-		"--color-error-50": "249 221 234", // #f9ddea
-		"--color-error-100": "246 209 228", // #f6d1e4
-		"--color-error-200": "244 198 221", // #f4c6dd
-		"--color-error-300": "238 163 200", // #eea3c8
-		"--color-error-400": "225 94 159", // #e15e9f
-		"--color-error-500": "212 25 118", // #D41976
-		"--color-error-600": "191 23 106", // #bf176a
-		"--color-error-700": "159 19 89", // #9f1359
-		"--color-error-800": "127 15 71", // #7f0f47
-		"--color-error-900": "104 12 58", // #680c3a
-		// surface | #495a8f 
-		"--color-surface-50": "228 230 238", // #e4e6ee
-		"--color-surface-100": "219 222 233", // #dbdee9
-		"--color-surface-200": "210 214 227", // #d2d6e3
-		"--color-surface-300": "182 189 210", // #b6bdd2
-		"--color-surface-400": "128 140 177", // #808cb1
-		"--color-surface-500": "73 90 143", // #495a8f
-		"--color-surface-600": "66 81 129", // #425181
-		"--color-surface-700": "55 68 107", // #37446b
-		"--color-surface-800": "44 54 86", // #2c3656
-		"--color-surface-900": "36 44 70", // #242c46
+		// error | #d21934 
+		"--color-error-50": "248 221 225", // #f8dde1
+		"--color-error-100": "246 209 214", // #f6d1d6
+		"--color-error-200": "244 198 204", // #f4c6cc
+		"--color-error-300": "237 163 174", // #eda3ae
+		"--color-error-400": "224 94 113", // #e05e71
+		"--color-error-500": "210 25 52", // #d21934
+		"--color-error-600": "189 23 47", // #bd172f
+		"--color-error-700": "158 19 39", // #9e1327
+		"--color-error-800": "126 15 31", // #7e0f1f
+		"--color-error-900": "103 12 25", // #670c19
+		// surface | #000000 
+		"--color-surface-50": "255 240 254", // #fff0fe
+		"--color-surface-100": "255 235 253", // #ffebfd
+		"--color-surface-200": "255 231 253", // #ffe7fd
+		"--color-surface-300": "255 216 251", // #ffd8fb
+		"--color-surface-400": "255 186 248", // #ffbaf8
+		"--color-surface-500": "255 157 245", // #FF9DF5
+		"--color-surface-600": "230 141 221", // #e68ddd
+		"--color-surface-700": "191 118 184", // #bf76b8
+		"--color-surface-800": "153 94 147", // #995e93
+		"--color-surface-900": "255 255 255", // #7d4d78
+		
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
+<div class="bg-gradient-to-b from-pink-200 to-white">
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Let's get cracking bones!</h1>
@@ -13,3 +13,26 @@
 		</ul>
 	</div>
 </div>
+</div>
+
+<!--SO faux code
+Right here we want our header make a component for that 
+
++ Header will include the following 
+    -Icon 
+    -Menu? How do i do that? 
+    -Title should be clickable and lead home
+
++We're going to want our animation component here 
+It should include a moving hello kitty pixel art going through it every so often kind of like the AHF Website
+
+So for the last sections we just want filler text tbh. -W-
+
+Component 
+
+Component 
+
+Footer
+
+
+-->
